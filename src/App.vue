38ae31b166ad32cbab0ad82460c7ed292f4fd52d
igor-86 <i.campoli86@gmail.com>
2022-11-17 12:13:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "AppVue",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+//Import SASS
+@use "./styles/general.scss" as*;
+@use "./styles/partials/variables" as*;
+</style>
