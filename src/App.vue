8a91cde7d70e4e27a5330de6ffbd5daa2b4988a3
igor-86 <i.campoli86@gmail.com>
2022-11-17 14:53:46 +0100

    Scaffolding Debug Sass

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,17 @@ export default {
 </script>
 
 <template>
-
+  <h3>Start your</h3>
 </template>
 
 <style lang="scss">
 //Import SASS
 @use "./styles/general.scss" as*;
 @use "./styles/partials/variables" as*;
+
+/* Debu Variables Sass */
+h3 {
+  font-size: $title-big;
+  color: $c-bigtitle;
+}
 </style>
